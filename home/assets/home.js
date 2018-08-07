687f5e5b1d9c1033e5e46149ccceb69adb3e5758
5820K <38088640+chuynh18@users.noreply.github.com>
2018-08-07 00:36:41 -0700

    added a setTimeout to the image switching for the grid

--- home/assets/home.js
+++ home/assets/home.js
@@ -5,6 +5,6 @@ const mouseover = function(image) {
     const existingImage = document.getElementsByClassName("image");
 
     for (let i = 0; i < existingImage.length; i++) {
-        existingImage[i].src = `assets/img/${image}`;
+        setTimeout(() => existingImage[i].src = `assets/img/${image}`, 60*i);
     }
 }
