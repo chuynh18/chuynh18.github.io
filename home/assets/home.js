16e3727e7c73447727a758e7da41383fbb52faf7
5820K <38088640+chuynh18@users.noreply.github.com>
2018-08-07 14:16:46 -0700

    info text stripe

--- home/assets/home.js
+++ home/assets/home.js
@@ -1,12 +1,21 @@
 "use strict";
 
-const mouseover = function(image) {
+const mouseover = function(image, showText) {
 
     const existingImage = document.getElementsByClassName("image");
+    const infoText = document.getElementsByClassName("infotext");
     
     for (let i = 0; i < existingImage.length; i++) {
         existingImage[i].alt = `assets/img/${image}`;
-        setTimeout(() => existingImage[i].src = `assets/img/${image}`, 70*i);
+        setTimeout(function() {
+            existingImage[i].src = `assets/img/${image}`;
+            if (showText) {
+                infoText[i].style.display = "";
+            } else {
+                infoText[i].style.display = "none";
+            }
+
+        }, 70*i);
     }
 }
 
