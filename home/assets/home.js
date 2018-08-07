ef74a51bf258d4b5b3f854fb13d9ca2c95e0b5c5
5820K <38088640+chuynh18@users.noreply.github.com>
2018-08-07 00:46:26 -0700

    add modal

--- home/assets/home.js
+++ home/assets/home.js
@@ -3,8 +3,27 @@
 const mouseover = function(image) {
 
     const existingImage = document.getElementsByClassName("image");
-
+    
     for (let i = 0; i < existingImage.length; i++) {
+        existingImage[i].alt = `assets/img/${image}`;
         setTimeout(() => existingImage[i].src = `assets/img/${image}`, 60*i);
     }
 }
+
+const openModal = function(image) {
+    const img = document.getElementById("modalImage");
+    img.src = `assets/img/${image}`;
+    img.alt = `assets/img/${image}`;
+
+    modal.style.display = "block";
+}
+
+document.getElementById("modalClose").onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('modal')) {
+        modal.style.display = "none";
+    }
+} 
