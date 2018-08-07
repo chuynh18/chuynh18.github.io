5d5c72ac99db7eddcc4403c59e6f9f3cb3702bed
5820K <38088640+chuynh18@users.noreply.github.com>
2018-08-06 22:31:55 -0700

    proof of concept mouseover.  no animations yet...  not sure how that'll work at this point

--- home/assets/home.js
+++ home/assets/home.js
@@ -1,2 +1,10 @@
 "use strict";
 
+const mouseover = function(image) {
+
+    const existingImage = document.getElementsByClassName("image");
+
+    for (let i = 0; i < existingImage.length; i++) {
+        existingImage[i].src = `assets/img/${image}`;
+    }
+}
